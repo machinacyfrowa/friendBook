1d08d6ac7a1f14014988c27ae014caf9a6fd1810
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-04-29 10:38:46 +0200

    working user register

--- register.php
+++ register.php
@@ -1,19 +1,27 @@
 <?php
+//zaimportuj kod klasy
+require_once('class/User.class.php');
 
+if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
+    //wysłano formularz - przechwyć i obrób dane
+    $email = $_REQUEST['email'];
+    $password = $_REQUEST['password'];
+    //wywołujemy metodę klasy
+    //metody statyczne w PHP wywołuje się poprzez ::
+    $result = User::Register($email, $password);
+}
 ?>
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formularz rejestracji</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
-    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
-    crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
-    crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <div id="container">
         <h1 class="text-center mt-5 mb-5">Zarejestruj konto:</h1>
@@ -26,8 +34,19 @@
                 <input type="password" class="form-control mb-3" name="password" id="passwordInput">
 
                 <button type="submit" class="btn btn-primary w-100 mt-3">Zarejestruj</button>
-            </form> 
+            </form>
+            <?php
+            if (isset($result)) {
+                if ($result) {
+                    echo "Udało się utworzyć konto";
+                } else {
+                    echo "Nie udało się utworzyć konta";
+                }
+            }
+
+            ?>
         </div>
     </div>
 </body>
+
 </html>
