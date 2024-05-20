372f1822bbd80bd49307a40279bc908e4def22e8
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-05-20 09:38:58 +0200

    profile class stub

---
+++ class/Profile.class.php
@@ -0,0 +1,35 @@
+<?php
+class Profile {
+    private int $_id;
+    private int $_userID;
+    private string $_firstName;
+    private string $_lastName;
+    private int $_profilePhotoID;
+
+    public function __construct(int $id, int $userID, string $firstName, string $lastName, int $profilePhotoID)
+    {
+        $this->_id = $id;
+        $this->_userID = $userID;
+        $this->_firstName = $firstName;
+        $this->_lastName = $lastName;
+        $this->_profilePhotoID = $profilePhotoID;
+    }
+
+    static function Get($id) : Profile {
+        //pobierz jeden profil po jego ID
+        //zwróć obiekt Profile
+        //połączenie do bazy danych
+        $db = new mysqli('localhost', 'root', '', 'friendbook');
+        //kwerenda do bazy danych
+        $sql = "SELECT * FROM profile WHERE ID=? LIMIT 1";
+        $q = $db->prepare($sql);
+        $q->bind_param("i", $id);
+        $q->execute();
+        $result = $q->get_result();
+        $row = $result->fetch_assoc();
+        $p = new Profile($row['ID'], $row['userID'], $row['firstName'], $row['lastName'], $row['profilePhotoID']);
+        return $p;
+    }
+}
+
+?>
